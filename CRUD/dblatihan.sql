e7a38a5aa6d2bd1bd7501b87087e60bdf263d37f
FoxBitExe <gardapramudya.gp@gmail.com>
2023-07-04 22:10:35 +0700

    Berisakan File belajar Web  dan Pemrograman

---
+++ CRUD/dblatihan.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2023 at 03:01 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dblatihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmhs`
+--
+
+CREATE TABLE `tmhs` (
+  `id_mhs` int(11) NOT NULL,
+  `peserta` varchar(9) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tmhs`
+--
+
+INSERT INTO `tmhs` (`id_mhs`, `peserta`, `nama`, `alamat`, `jurusan`) VALUES
+(1, '17830100', 'Garda Pramudya', 'Condongcatur, Depok, Sleman', 'TKJ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tmhs`
+--
+ALTER TABLE `tmhs`
+  ADD PRIMARY KEY (`id_mhs`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tmhs`
+--
+ALTER TABLE `tmhs`
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ CRUD/index.php
@@ -0,0 +1,180 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $pass = "";
+    $database = "dblatihan";
+
+    $koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
+    if(isset($_POST['bsimpan']))
+    {
+      if($_GET['hal'] == "edit")
+      {
+        $edit = mysqli_query($koneksi, " UPDATE tmhs set 
+                                          peserta = '$_POST[tpeserta]',
+                                          nama = '$_POST[tnama]',
+                                          alamat = '$_POST[talamat]',
+                                          jurusan = '$_POST[tjurusan]'
+                                        WHERE id_mhs = '$_GET[id]'
+                                        ");
+
+        if($edit)
+        {
+        echo "<script>
+                alert('DATA ANDA TELAH BERHASIL DI RUBAH');
+                document.location='index.php';
+              </script>";
+        }
+        else
+        {
+          echo "<script>
+                  alert('DATA ANDA TELAH GAGAL DIRUMAH');
+                  document.location='index.php';
+                </script>";
+        }
+      }
+      else
+      {
+        $simpan = mysqli_query($koneksi, "INSERT INTO tmhs (peserta, nama, alamat, jurusan)
+        VALUE ('$_POST[tpeserta]', 
+              '$_POST[tnama]', 
+              '$_POST[talamat]', 
+              '$_POST[tjurusan]')
+        ");
+
+        if($simpan)
+        {
+        echo "<script>
+                alert('DATA ANDA TELAH TERSIMPAN');
+                document.location='index.php';
+              </script>";
+        }
+        else
+        {
+          echo "<script>
+                  alert('DATA ANDA GAGAL TERSIMPAN');
+                  document.location='index.php';
+                </script>";
+        }
+      }
+      
+    }
+
+    if(isset($_GET['hal']))
+    {
+      if($_GET['hal'] == "edit")
+      {
+        $tampil = mysqli_query($koneksi, "SELECT * FROM tmhs WHERE id_mhs = '$_GET[id]' ");
+        $data = mysqli_fetch_array($tampil);
+        if($data)
+        {
+          $vpeserta = $data['peserta'];
+          $vnama = $data['nama'];
+          $valamat = $data['alamat'];
+          $vjurusan = $data['jurusan'];
+        }
+      }
+      else if ($_GET['hal'] == "hapus")
+      {
+        $hapus = mysqli_query($koneksi, "DELETE FROM tmhs WHERE id_mhs = '$_GET[id]' ");
+        if($hapus)
+          {
+            echo "<script>
+                    alert('HAPUS DATA SUKSES');
+                    document.location='index.php';
+                  </script>";
+          }
+      }
+
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BELAJAR CRUD</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="">
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="text-center">BELAJAR CRUD</h1>
+    <h2 class="text-center">Admin Panel</h2>
+
+    <div class="card mt-3">
+      <div class="card-header bg-primary text-white ">
+        FORM INPUT DATA
+      </div>
+      <div class="card-body">
+      <form method="post" action="">
+          <div class="form-group">
+              <label>NOMOR PERSERTA</label>
+              <input type="text" name="tpeserta" value="<?=@$vpeserta?>" class="form-control" placeholder="input nim anda disini" required>
+          </div>
+          <div class="form-group">
+            <label>NAMA PESERTA</label>
+            <input type="text" name="tnama" value="<?=@$vnama?>"  class="form-control" placeholder="input nama anda disini" required>
+          </div>
+          <div class="form-group">
+            <label>ALAMAT</label>
+            <textarea class="form-control" name="talamat" placeholder="inpur alamat anda disni!"><?=@$valamat?></textarea>
+          </div>
+          <div class="form-group">
+            <label>JURUSAN </label>
+            <select class="form-control" name="tjurusan">
+              <option value="<?=@$vjurusan?>"><?=@$vjurusan?></option>
+              <option value="TKJ">TKJ</option>
+              <option value="MM">MM</option>
+              <option value="RPL">RPL</option>
+            </select>
+          </div>
+
+          <button type="submit" class="btn btn-success" name="bsimpan">SIMPAN</button>
+          <button type="reset" class="btn btn-danger" name="breset">KOSONGKAN</button>
+
+        </form>
+      </div>
+    </div>
+
+    <div class="card mt-3">
+      <div class="card-header bg-success text-white ">
+        DAFTAR MAHASISWA
+      </div>
+      <div class="card-body">
+          <table class="table table-bordered table-striped">
+            <tr>
+                <th>No.</th>
+                <th>NOMOR PESERTA</th>
+                <th>NAMA</th>
+                <th>ALAMAT</th>
+                <th>JURUSAN</th>
+                <th>Aksi</th>
+            </tr>
+            <?php
+                $no = 1;
+                $tampil = mysqli_query($koneksi, "SELECT * from tmhs order by id_mhs desc");
+                while($data = mysqli_fetch_array($tampil)) :
+            ?>
+            <tr>
+              <td><?=$no++;?></td>
+              <td><?=$data['peserta']?></td>
+              <td><?=$data['nama']?></td>
+              <td><?=$data['alamat']?></td>
+              <td><?=$data['jurusan']?></td>
+              <td>
+                <a href="index.php?hal=edit&id=<?=$data['id_mhs']?>" class="btn btn-warning"> Edit </a>
+                <a href="index.php?hal=hapus&id=<?=$data['id_mhs']?>" onclick="return confirm ('APAKAH ANDA YAKIN INGIN MENGHAPUS DATA INI?')" class="btn btn bg-danger"> Hapus </a>
+              </td>
+            </tr>
+            <?php endwhile; ?>
+          </table>
+      </div>
+    </div>
+
+  </div>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
